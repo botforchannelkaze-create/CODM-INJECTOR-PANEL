a88d65c77d6a7040c9d507bd5bc5bf020218a424
botforchannelkaze-create <botforchannelkaze@gmail.com>
2026-03-11 18:00:32 +0800

    Create Panel.py

---
+++ Panel.py
@@ -0,0 +1,36 @@
+from flask import Flask, request
+import uuid, time
+
+app = Flask(__name__)
+
+keys = {}
+
+@app.route("/getkey")
+def getkey():
+    key = str(uuid.uuid4())
+    keys[key] = {"expiry": time.time()+86400, "device": None}
+    return f"YOUR KEY: {key}"
+
+@app.route("/verify")
+def verify():
+    key = request.args.get("key")
+    device = request.args.get("device")
+
+    if key not in keys:
+        return "invalid"
+
+    data = keys[key]
+
+    if time.time() > data["expiry"]:
+        return "expired"
+
+    if data["device"] is None:
+        data["device"] = device
+        return "valid"
+
+    if data["device"] == device:
+        return "valid"
+
+    return "locked"
+
+app.run(host="0.0.0.0", port=10000)
